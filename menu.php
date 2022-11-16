a2d8ac0001627a8e70312e792a684f07c6dcb509
CasandraPH <78100450+CasandraPH@users.noreply.github.com>
2022-11-16 00:34:20 -0600

    Parte lateral izquierda de la página de inicio.
    Muestra la opción de reservas en la página de inicio.

---
+++ menu.php
@@ -0,0 +1,66 @@
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+
+      <ul class="sidebar-menu">
+        
+        <li>
+          <a href="http://localhost/restaurante/Inicio">
+            <i class="fa fa-user"></i>
+            <span>Reservas</span>
+          </a>
+        </li>
+
+        <<!-- li>
+          <a href="http://localhost/restaurante/Usuarios">
+            <i class="fa fa-users"></i>
+            <span>Usuarios</span>
+          </a>
+        </li> -->
+
+        <!-- <li class="treeview">
+          
+          <a href="#">
+            
+            <i class="fa fa-list-ul"></i>
+            <span>Menú</span>
+
+            <span class="pull-right-container">
+              
+              <i class="fa fa-angle-left pull-right"></i>
+
+            </span>
+
+          </a>
+
+          <ul class="treeview-menu">
+          
+            <li>
+              <a href="#">
+                <span>Categorías</span>
+              </a>
+            </li>
+
+            <li>
+              <a href="#">
+                <span>Comidas</span>
+              </a>
+            </li>
+
+          </ul>
+
+        </li> -->
+
+        
+        <!-- <li>
+          <a href="#">
+            <i class="fa fa-motorcycle"></i>
+            <span>Delivery</span>
+          </a>
+        </li>
+
+      </ul> -->
+    
+    </section>
+    <!-- /.sidebar -->
+  </aside>
