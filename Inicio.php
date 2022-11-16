4fae9f3a9b7ac736e5cbc493dcfab3c4f5318d92
robertomigranas <100160597+robertomigranas@users.noreply.github.com>
2022-11-16 00:29:55 -0600

    Pagina de inicio
    Muestra las mesas que están disponibles y las que están ocupadas.

---
+++ Inicio.php
@@ -0,0 +1,189 @@
+<div class="content-wrapper">
+	
+	<section class="content-header">
+		
+		<h1>Reservas</h1>
+
+	</section>
+
+	<section class="content">
+
+		<h1>Mapa de Ubicación de Mesas</h1>
+
+		<h3>Selecciona la mesa de su preferencia</h3>
+
+		<br><br>
+		
+		<div class="row">
+			
+			<div class="col-md-3 col-xs-6 bg-success">
+				
+				<h2>Mesa Número: 1 </h2>
+
+				<h3>Capacidad: 4 Personas</h3>
+
+				<h3>Status: <button class="btn btn-success">Disponible</button></h3>
+
+				<a href="#">
+					
+					<button class="btn btn-primary" data-toggle="modal" data-target="#CrearMesa">Reservar</button>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-danger">
+				
+				<h2>Mesa Número: 2 </h2>
+
+				<h3>Capacidad: 2 Personas</h3>
+
+				<h3>Status: <button class="btn btn-danger">No disponible</button></h3>
+
+				<a href="#">
+					
+					<br>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-success">
+				
+				<h2>Mesa Número: 3 </h2>
+
+				<h3>Capacidad: 6 Personas</h3>
+
+				<h3>Status: <button class="btn btn-success">Disponible</button></h3>
+
+				<a href="#">
+					
+					<button class="btn btn-primary" data-toggle="modal" data-target="#CrearMesa">Reservar</button>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-success">
+				
+				<h2>Mesa Número: 4 </h2>
+
+				<h3>Capacidad: 4 Personas</h3>
+
+				<h3>Status: <button class="btn btn-success">Disponible</button></h3>
+
+				<a href="#">
+					
+					<button class="btn btn-primary" data-toggle="modal" data-target="#CrearMesa">Reservar</button>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-danger">
+				
+				<h2>Mesa Número: 5 </h2>
+
+				<h3>Capacidad: 8 Personas</h3>
+
+				<h3>Status: <button class="btn btn-danger">No disponible</button></h3>
+
+				<a href="#">
+					
+					<br>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-danger">
+				
+				<h2>Mesa Número: 6 </h2>
+
+				<h3>Capacidad: 10 Personas</h3>
+
+				<h3>Status: <button class="btn btn-danger">No disponible</button></h3>
+
+				<a href="#">
+					
+					<br>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-success">
+				
+				<h2>Mesa Número: 7 </h2>
+
+				<h3>Capacidad: 4 Personas</h3>
+
+				<h3>Status: <button class="btn btn-success">Disponible</button></h3>
+
+				<a href="#">
+					
+					<button class="btn btn-primary" data-toggle="modal" data-target="#CrearMesa">Reservar</button>
+
+				</a>
+
+			</div>
+
+			<div class="col-md-3 col-xs-6 bg-danger">
+				
+				<h2>Mesa Número: 8 </h2>
+
+				<h3>Capacidad: 4 Personas</h3>
+
+				<h3>Status: <button class="btn btn-danger">No disponible</button></h3>
+
+				<a href="#">
+					
+					<br>
+
+				</a>
+
+			</div>
+
+
+		</div>
+
+	</section>
+
+</div>
+
+
+
+
+<div class="modal fade" id="CrearMesa">
+	
+	<div class="modal-dialog">
+		
+		<div class="modal-content">
+			
+			<form method="post">
+				
+				<div class="modal-body">
+					
+					<div class="box-body">
+						
+						<div class="form-group">
+							
+							<h2>¡Gracias por tu reserva!</h2>
+
+				</div>
+
+
+				<div class="modal-footer">
+
+					<button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+
+				</div>
+
+			</form>
+
+		</div>
+
+	</div>
+
+</div>
+
